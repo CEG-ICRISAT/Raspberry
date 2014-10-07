b83f83c234affa093d063996eda1d18067df4958
AVSK Mohan Katta <srikrishnamohan@gmail.com>
2014-10-07 16:54:40 +0530

    fixed parallelization issue

--- qc.c
+++ qc.c
@@ -1,15 +1,21 @@
+/*
+Raspberry, a qc tool for processing large sets of fastq files quickly
+Authors:  AVSK Mohan Katta, Aamir  Khan, Rajeev Varshney
+Center of Excellence in Genomics
+ICRISAT, India
+License GPL V3
+*/
 #include<stdio.h>
 #include<omp.h>
 #include "qc.h"
 COUNTS_INIT(OFFSET);
 int main(int argc, char** argv){
 int k;
-#pragma omp parallel num_threads(10)
+#pragma omp parallel
 {
 #pragma omp for ordered 
 for(k=1; k < argc; ++k){
-        #pragma omp ordered
-        stats(argv[k]);
+    stats(argv[k]);
 }
 }
 return 0;
